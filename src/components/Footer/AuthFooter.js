4bd33a2b19bdca3461a7c9da62a2711c27d87433
Sterczik <sterczewskik@gmail.com>
2019-06-29 22:54:59 +0200

    Add Auth layout and elements | Add login page

---
+++ src/components/Footer/AuthFooter.js
@@ -0,0 +1,66 @@
+/*eslint-disable*/
+import React from 'react'
+import { NavItem, NavLink, Nav, Container, Row, Col } from 'reactstrap'
+
+class Login extends React.Component {
+  render() {
+    return (
+      <>
+        <footer className="py-5">
+          <Container>
+            <Row className="align-items-center justify-content-xl-between">
+              <Col xl="6">
+                <div className="copyright text-center text-xl-left text-muted">
+                  © {new Date().getFullYear()}{" "}
+                  <a
+                    href="!#"
+                    target="_blank"
+                  >
+                    Sterczik
+                  </a>{" | "}
+                  Theme created by {" "}
+                  <a
+                    href="https://www.creative-tim.com"
+                    target="_blank"
+                  >
+                    Creative Tim
+                  </a>
+                </div>
+              </Col>
+              <Col xl="6">
+                <Nav className="nav-footer justify-content-center justify-content-xl-end">
+                  <NavItem>
+                    <NavLink
+                      href="!#"
+                      target="_blank"
+                    >
+                      Github
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      href="!#"
+                      target="_blank"
+                    >
+                      About
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      href="!#"
+                      target="_blank"
+                    >
+                      Blog
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+              </Col>
+            </Row>
+          </Container>
+        </footer>
+      </>
+    )
+  }
+}
+
+export default Login
